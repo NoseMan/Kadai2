28f11fa258013e84e1a2527cb991123939d68abb
g13958ya <g13958ya@gm.tsuda.ac.jp>
2016-04-15 11:44:52 +0900

    名前とグループ名を追加したよ

--- src/com/toy/anagrams/ui/Anagrams.java
+++ src/com/toy/anagrams/ui/Anagrams.java
@@ -165,7 +165,7 @@ public class Anagrams extends JFrame {
         gridBagConstraints.insets = new java.awt.Insets(0, 0, 20, 0);
         mainPanel.add(guessedWord, gridBagConstraints);
 
-        feedbackLabel.setText("aaaaaaaaaaaaaa");
+        feedbackLabel.setText("島田優里、谷中敦子、矢野夏希（マイメロvsキキララ）");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 4;
